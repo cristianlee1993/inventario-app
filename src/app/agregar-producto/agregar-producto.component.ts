dea41bda8774c330bf936abf530677c96b7fc4da
CristianLee <125401179+cristianlee1993@users.noreply.github.com>
2024-08-08 14:09:15 -0600

    proyecto nuevo

---
+++ src/app/agregar-producto/agregar-producto.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Producto } from '../producto';
+import { ProductoService } from '../producto.service';
+import { Subscriber } from 'rxjs';
+
+@Component({
+  selector: 'app-agregar-producto',
+  templateUrl: './agregar-producto.component.html',
+  styleUrls: ['./agregar-producto.component.css']
+})
+
+export class AgregarProductoComponent {
+
+  producto: Producto = new Producto();
+
+  constructor(private productoServicio: ProductoService,
+     private enrutador: Router) {}
+
+  onSubmit(){
+    this.guardarProducto();
+  }
+
+  guardarProducto(){
+    this.productoServicio.agregarProducto(this.producto).subscribe(
+      {
+        next: (datos) => {
+          this.irListaProductos();
+        },
+        error:(error: any )=> {console.log(error)}
+
+      }
+    );
+  }
+
+  irListaProductos(){
+    this.enrutador.navigate(['/productos']);
+  }
+
+}
---
+++ src/app/editar-producto/editar-producto.component.ts
@@ -0,0 +1,52 @@
+import { ProductoService } from './../producto.service';
+import { Component } from '@angular/core';
+import { Producto } from '../producto';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-editar-producto',
+  templateUrl: './editar-producto.component.html',
+
+})
+
+export class EditarProductoComponent {
+
+  producto: Producto = new Producto();
+  id: number;
+
+  constructor (private ProductoService: ProductoService,
+    private ruta: ActivatedRoute,
+    private enrutador: Router){}
+
+  ngOnInit(){
+
+    this.id = this.ruta.snapshot.params['id'];
+    this.ProductoService.obtenerProductoPorId(this.id).subscribe(
+
+      {
+        next:(datos) => this.producto=datos,
+        error: (errores: any ) => console.log(errores)
+      }
+    );
+  }
+
+  onSubmit(){
+    this.editarProducto();
+  }
+
+  editarProducto(){
+    this.ProductoService.editarProducto(this.id, this.producto).subscribe(
+      {
+        next: (datos) => this.irProductoLista(),
+        error: (errores) => console.log(errores)
+
+      }
+    );
+  }
+
+  irProductoLista(){
+    this.enrutador.navigate(['/productos'])
+  }
+
+
+}
